5dacb12e91c74d4e4700c58c6fe45d25c96432c1
Victor <87349107+Vycttor@users.noreply.github.com>
2024-11-21 23:45:55 -0300

    Aprendendo JavaScript

---
+++ aula11/ex008.js
@@ -0,0 +1,18 @@
+//Aprendendo a prgramar em javaScript.
+
+var vel = 55.5
+var soma = 0
+var n1 = 5
+var n2 = 5
+soma = n1 + n2
+
+console.log('O console funcionou corretamente!')
+if(vel > 60){ 
+    console.log(`A velocidade do seu carro é ${vel} km/h, MULTADO.`) 
+}else{
+    console.log(`Velocidade de ${vel}, dentro do limite.`)
+}
+
+
+
+//console.log(`A soma de ${n1} + ${n2} = ${soma}`)
---
+++ aula11/ex009.js
@@ -0,0 +1,11 @@
+//Aula 9, aprendendo JavaScript
+
+var pais = 'US'
+console.log(`Vivendo em ${pais} `)
+
+//Condição composta pois há um IF e um ELSE
+if(pais == 'Brasil'){
+    console.log('Você é Brasileiro')
+}else{
+    console.log(`Você é Estrangeiro!`)
+}

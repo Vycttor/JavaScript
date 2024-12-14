e4cb0192963d6fc1c00e5a2e31e07241c681ab1b
Victor <87349107+Vycttor@users.noreply.github.com>
2024-12-14 00:03:17 -0300

    Atualizado aula16ex

---
+++ aula16ex/modelo.js
@@ -0,0 +1,8 @@
+let num = document.querySelector('input#fnum')
+let lista = document.getElementById('flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function adicionar(){
+
+}//Function
